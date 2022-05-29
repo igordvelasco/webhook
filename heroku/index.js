8379fe13c36f3b64b01139bd3b9098020d8e258f
igordvelasco <66185381+igordvelasco@users.noreply.github.com>
2022-05-29 13:26:37 -0300

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -37,8 +37,6 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 });
 
 app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
-
   if (!req.isXHubValid()) {
     console.log('Warning - request header X-Hub-Signature not present or invalid');
     res.sendStatus(401);
@@ -46,7 +44,37 @@ app.post('/facebook', function(req, res) {
   }
 
   console.log('request header X-Hub-Signature validated');
-  // Process the Facebook updates here
+
+  const message = req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0]?.text?.body;
+  const phone = req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0]?.from;
+
+  let template;
+
+  switch (message) {
+    case 'Conhecer os projetos':
+    case 'Problemas com plataforma':
+    case 'Formação de professores':
+    default:
+      template = 'boas_vindas'
+  }
+  
+  await fetch('https://graph.facebook.com/v13.0/100679882671832/messages', {
+    method: 'post',
+    body: JSON.stringify({
+      "messaging_product": "whatsapp",
+      "to": phone,
+      "type": "template",
+      "template": {
+        "name": template,
+        "language": { "code": "pt_BR" }
+      }
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${jwt}`
+    }
+  });
+
   received_updates.unshift(req.body);
   res.sendStatus(200);
 });
